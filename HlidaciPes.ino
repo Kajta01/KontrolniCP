9a79d01dbf692857685a9bb54733ec46330348eb
Kajta01 <37844835+Kajta01@users.noreply.github.com>
2021-04-01 10:11:02 +0200

    wtg disable

--- HlidaciPes.ino
+++ HlidaciPes.ino
@@ -2,36 +2,77 @@
 
 //https://wolles-elektronikkiste.de/en/watchdog-timer
 
-
-void WDT_setup(){
+void WDT_setup()
+{
   watchdogSetup();
 }
 
-void WDT_loop(){
+void WDT_loop()
+{
   delay(500);
   wdt_reset();
-  endlessLoop(); 
+  endlessLoop();
 }
 
-void watchdogSetup(void){
+void watchdogSetup(void)
+{
+#if DEBUG
+  Serial.println("watchdogSetup - Start");
+  delay(100);
+#endif
   wdt_reset();
-  wdt_enable(WDTO_8S); 
-  WDTCSR = (1<<WDIE); // interrupt
+  wdt_enable(WDTO_8S);
+  WDTCSR = (1 << WDIE); // interrupt
 }
 
-void hardwareReset(){
-  pinMode(RESET,OUTPUT);
-  digitalWrite(RESET,HIGH);
+void hardwareReset()
+{
+  pinMode(RESET, OUTPUT);
+  digitalWrite(RESET, HIGH);
 }
 
-void endlessLoop(){
+void endlessLoop()
+{
   int j = 0;
-  while(2 < 5){
-    Serial.println(j++);   
+  while (2 < 5)
+  {
+    Serial.println(j++);
     delay(1000);
   }
 }
 
-ISR(WDT_vect){
+ISR(WDT_vect)
+{
+  #if DEBUG
+  Serial.println("hardwareReset");
+  delay(100);
+#endif
   hardwareReset();
+}
+
+void WTG_disable()
+{
+#if DEBUG
+  Serial.println("wdt_disable");
+  delay(100);
+#endif
+  wdt_disable();
+}
+
+void WTG_enable()
+{
+#if DEBUG
+  Serial.println("WTG_enable");
+  delay(100);
+#endif
+  watchdogSetup();
+}
+
+void WTG_reset()
+{
+#if DEBUG
+  Serial.println("wdt_reset");
+  delay(100);
+#endif
+  wdt_reset();
 }
